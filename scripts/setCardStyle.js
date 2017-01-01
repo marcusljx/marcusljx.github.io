36108a8a5e9eb8b46de0a9113fe26f5f2b5e0738
marcusljx <marcusljx@gmail.com>
2017-01-02 07:58:48 +0800

    Moved card representation to JS. Added cards.

--- scripts/setCardStyle.js
+++ scripts/setCardStyle.js
@@ -2,8 +2,14 @@
  * Created by marcusljx on 31/12/16.
  */
 
-var cards = document.getElementsByClassName("resume-card");
+var rCards = document.getElementsByClassName("resume-card");
 
-for (i = 0; i< cards.length; i++) {
-    cards[i].className += " mdc-card mdc-elevation--z5 mdc-ripple-surface mdc-ripple-upgraded";
+for (i = 0; i< rCards.length; i++) {
+    rCards[i].className += " mdc-card mdc-elevation--z5 mdc-ripple-surface mdc-ripple-upgraded";
+}
+
+var iCards = document.getElementsByClassName("item-card");
+
+for (i = 0; i< iCards.length; i++) {
+    iCards[i].className += " mdc-card mdc-elevation--z5 mdc-ripple-surface mdc-ripple-upgraded";
 }
