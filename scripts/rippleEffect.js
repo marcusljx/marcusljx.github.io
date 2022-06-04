f9e87ff3964abfdd2b27c190a53d7b059dabea7f
Marcus Low Junxiang <marcusljx@gmail.com>
2022-06-04 17:40:38 +0000

    Removing older files

--- scripts/rippleEffect.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by marcusljx on 31/12/16.
- */
-
-(function(global) {
-    [].forEach.call(document.querySelectorAll('.mdc-ripple-surface'), function(surface) {
-        mdc.ripple.MDCRipple.attachTo(surface);
-    });
-})(this);
